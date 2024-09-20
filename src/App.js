ea8faab98ae3f0ae2060f26df5b4b112e1f21251
Anil Itagi <itagianil464@gmail.com>
2024-09-20 14:50:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState("Welcome to my React app!");
+
+  const handleClick = () => {
+    setMessage("You clicked the button!");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message}</h1>
+      <button onClick={handleClick}>Click Me!</button>
     </div>
   );
 }
